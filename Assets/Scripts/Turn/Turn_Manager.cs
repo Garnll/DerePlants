820e0f3f9c40cd5a0b7c5779074dc76d9ca6dbad
DESKTOP-HGDOKOS\Zlaster <zlaster@hotmail.com>
2017-10-26 13:50:03 -0500

    Sistema de Cambio de Turnos Automáticos
    Turn_Manager cuenta con dos valores constantes: TOTAL_TURNS para el total de turnos y TURN_TIME para el tiempo que dura cada turno.  Tiene tres funciones nuevas:  switchTurn(), activatePlayer() y la corrutina turn(). En activatePlayer se cambia el atributo onTurn de los Player.

--- Assets/Scripts/Turn/Turn_Manager.cs
+++ Assets/Scripts/Turn/Turn_Manager.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class Turn_Manager : MonoBehaviour {
 
+	public const int TOTAL_TURNS = 10;
+	public const float TURN_TIME = 5.0f;
+
 	public Player[] players = new Player[2];
-
-	int currentTurn;
 	Player currentPlayer;
-
-
+	int currentTurn;
 
 	void Start () {
 		currentTurn = 1;
@@ -17,22 +17,33 @@ public class Turn_Manager : MonoBehaviour {
 		players[0] = new Player(1, new HumanBehaviour());
 		players[1] = new Player(2, new CpuBehaviour());
 
-		currentPlayer = players[0];
-		players[0].activate();
-
+		activatePlayer(players[0]);
+		StartCoroutine(turn());
 	}
-	
-	void Update () {
-		currentPlayer.act();
-		Debug.Log(currentPlayer.id);
 
-		if (Input.GetKeyDown(KeyCode.A)){
-			if (currentPlayer == players[0]) {
-				currentPlayer = players[1];
-			}
-			else {
-				currentPlayer = players[0];
-			}
-		} 
+	IEnumerator turn() {
+		while (currentTurn < TOTAL_TURNS) {
+			yield return new WaitForSeconds(TURN_TIME);
+			switchTurn();
+		}
 	}
+
+	void activatePlayer(Player player) {
+		try { currentPlayer.deactivate(); } catch { }
+		currentPlayer = player;
+		currentPlayer.activate();
+		Debug.Log("Turn " + currentTurn + ", Player " + currentPlayer.id + " at " + Time.time + ".");
+	}
+
+	void switchTurn() {
+		currentTurn++;
+		if (currentPlayer == players[0]) {
+			activatePlayer(players[1]);
+		}
+		else {
+			activatePlayer(players[0]);
+		}
+	}
+
+
 }
