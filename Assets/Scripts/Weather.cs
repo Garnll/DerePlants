a505a415f685e5b81250fa48bf3d9a2e5b3b91d9
DESKTOP-HGDOKOS\Zlaster <zlaster@hotmail.com>
2017-10-27 12:22:21 -0500

    Weather + WeatherManager
    La clase Weather define los diferentes tipos de climas que pueden haber. WeatherManager los administra en un arreglo de Weathers, define sus magnitudes y se encarga de cambiarlos.  fillWeathersArray() llena el arreglo según un enum de nombres. fillProbabilityMatrix() utiliza una constante WEATHER0_P que define la probabilidad de que el clima retorne al clima 0 (Primavera).
    fillAcumulativeMatrix() define los intervalos numéricos que le pertenecen a cada clima posible, de forma que generando un random entre 0 y 1 se sepa qué clima prosigue.
    Las probabilidades siempre dependen de WEATHER0_P, dado que el resto de las probabilidades se dividen por igual entre los demás climas posibles.
    Para añadir un clima nuevo hay que añadir el nombre del mismo en el enum WeatherName y su magnitud en el int[] magnitudes.

---
+++ Assets/Scripts/Weather.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weather {
+
+	public int id;
+	public string name;
+	public int magnitude;
+	public int turnsBeing;
+
+	public Weather(int i, int m, string n) {
+		id = i;
+		magnitude = m;
+		name = n;
+	}
+
+	public void reset() {
+		turnsBeing = 0;
+	}
+
+	public int getDealingDamage() {
+		return (5 * (magnitude + turnsBeing) / 3); 
+	}
+
+
+}
---
+++ Assets/Scripts/WeatherManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherManager : MonoBehaviour {
+
+	const float WEATHER0_P = 0.4f;
+
+	Weather[] weathers;
+	enum WeatherName				{ Spring, Summer, Autumn, Winter};
+	int[] magnitudes = new int[]	{ 0,	  5,	  3,	  6		};
+
+	float[,] probabilityMatrix;
+	float[,] acumulativeDist;
+
+	public Weather currentWeather;
+
+	void Start () {
+		fillWeathersArray();
+		fillProbabilityMatrix();
+		fillAcumulativeDist();
+		currentWeather = weathers[0];
+	}
+
+	void switchWeather() {
+		float r = Random.Range(0, 1);
+	}
+
+	string[] getWeatherNames() {
+		string[] weatherNames = System.Enum.GetNames(typeof(WeatherName));
+		return weatherNames;
+	}
+
+	float getProbabilityOf(int i, int j) {
+		if (j == 0) {
+			return WEATHER0_P;
+		}
+		else {
+			return (1 - WEATHER0_P) / (weathers.Length - 1);
+		}
+	}
+
+	void fillWeathersArray() {
+		string[] weatherNames = getWeatherNames();
+		//Debug.Log("There are " + weatherNames.Length + " weathers.");
+		weathers = new Weather[weatherNames.Length];
+		for (int i = 0; i < weatherNames.Length; i++) {
+			weathers[i] = new Weather(i, magnitudes[i], weatherNames[i]);
+			Debug.Log("Created " + weathers[i].name + " weather with id " + weathers[i].id + " and magnitude " + weathers[i].magnitude + ".");
+		}
+	}
+
+	void fillProbabilityMatrix() {
+		probabilityMatrix = new float[weathers.Length, weathers.Length];
+		for (int i = 0; i < weathers.Length; i++) {
+			for (int j = 0; j < weathers.Length; j++) {
+				probabilityMatrix[i, j] = getProbabilityOf(i, j);
+				//Debug.Log("The probability to get to " + weathers[j].name + " from " 
+				//	+ weathers[i].name + " is " + probabilityMatrix[i, j] + ".");
+			}
+		}
+	}
+
+	void fillAcumulativeDist() {
+		acumulativeDist = new float[probabilityMatrix.Length, probabilityMatrix.Length];
+		for (int i = 0; i<weathers.Length; i++) {
+			float sum = 0;
+			for (int j=0; j<weathers.Length; j++) {
+				Debug.Log(i + "," + j);
+				sum += probabilityMatrix[i, j];
+				acumulativeDist[i, j] = sum;
+				if (j > 0) {
+					Debug.Log("If " + acumulativeDist[i, j - 1] + " < r < " + acumulativeDist[i, j]
+						+ " then it will be " + weathers[j].name + ".");
+				}
+			}
+		}
+	}
+}
