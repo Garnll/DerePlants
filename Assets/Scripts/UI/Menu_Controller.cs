803685e7ac43b053da2ff93bb4a57e97e9a47fb9
Estudiante Bloque 10 Sala 210  B10S210est <B10S210est@AULASMED.UPB>
2017-11-07 14:29:54 -0500

    Creado Menu_Controller.cs
    Creado Script, Menu_Controller

---
+++ Assets/Scripts/UI/Menu_Controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class Menu_Controller : MonoBehaviour {
+
+    public string scene1, scene2, scene3;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    void PassScene()
+    {
+      
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
