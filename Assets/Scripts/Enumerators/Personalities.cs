d3868220fd6db0d9c66a3f11b06885b39070d868
DESKTOP-HGDOKOS\Zlaster <zlaster@hotmail.com>
2017-11-09 13:33:38 -0500

    Plantas Procedurales
    Ahora las plantas se generan aleatoriamente con los assets de PlantCreator.
    Falta modificar su comportamiento dependiendo de su personalidad y cambiar sus caras.

---
+++ Assets/Scripts/Enumerators/Personalities.cs
@@ -0,0 +1,3 @@
+﻿
+
+public enum PersonalityType { Tsundere, Normie }
--- Assets/Scripts/Plant/Graphical/Graphical_Component.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[RequireComponent(typeof(SpriteRenderer))]
-public class Graphical_Component : MonoBehaviour {
-
-
-}
