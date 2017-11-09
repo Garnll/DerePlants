d6b91d076d45b47b7cee3d608335cbb8986b6692
MrD00M <camilo.villegas@upb.edu.co>
2017-11-09 13:40:17 -0500

    Highscore0.2 - InputField funcional
    Cuando el jugador escribe su nombre (3 letras) y presiona Enter, este se guarda como un nuevo nombre.

---
+++ Assets/Scripts/UI/Highscore_Manager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Highscore_Manager : MonoBehaviour {
+
+    public float newHeight, oldHeight;
+    public string newName, oldName;
+    public Turn_Manager currentTurn;
+
+    [SerializeField]
+    private Text name;
+
+ 
+
+    public void AddName()
+    {
+        newName = name.text.ToString();
+        Debug.Log(newName);
+
+    for (int i= 0;i< 10;i++)
+        {
+            if (PlayerPrefs.HasKey(i + "HSCore"))
+            {
+            
+                    //oldHeight = PlayerPrefs.GetInt(i + "HScore");
+                    oldName = PlayerPrefs.GetString(i + "HScoreName");
+                   // PlayerPrefs.SetInt(i + "HScore", newHeight);
+                    PlayerPrefs.SetString(i + "HScoreName", newName);
+
+
+                             
+            }
+
+        }
+
+
+    }
+
+}
